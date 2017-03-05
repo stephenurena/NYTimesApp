9f20568da1b8cf63f84f35755a2e1af6ac3617b8
Stephen Urena <surena@Stephens-MacBook-Air.local>
2017-03-04 16:04:31 -0800

    modifed js file to add apprx read time

--- nytApp.js
+++ nytApp.js
@@ -1,9 +1,10 @@
 //Global Variables
 //==================================================================================
 var numArticle = 0;
+var limit = 0;
 //Functions
 //==================================================================================
-function nyApiQuery(searchTerm, startYear, endYear) {
+function nyApiQuery(numLimit, searchTerm, startYear, endYear) {
 
 //ajax and div creation
 //----------------------------------------------------------------------------------
@@ -24,33 +25,24 @@ function nyApiQuery(searchTerm, startYear, endYear) {
 				.done(function(data) {
 					console.log(data);
 					var response = data.response;
-
-				    for (var i = 0; i < 10; i++){
-				    	numArticle++
-				    	var articles = $("<div class='panel-body border displayArts'>");
-				    	var numberArt = $("<span class='label label-primary'>")
-					    	.text(numArticle)
-					    	articles.append(numberArt);
-				    	var headers = $("<h3>")
-					    	.text(response.docs[i].headline.main);
-					    	articles.append(headers);
-					    var section = $("<h5>")
-					    	.text("Section: " + response.docs[i].section_name)
-					    	articles.append(section);
-					    var author = $("<h5>")
-					    	.text(response.docs[i].byline.original)
-					    	articles.append(author);
-					    var date = $("<h5>")
-					    	.text(response.docs[i].pub_date.slice(0,10))
-					    	articles.append(date);
-				    	var p = $("<p>")
-					    	.text(response.docs[i].snippet);
-					    	articles.append(p);
-				    	var a = $("<a>")
-					    	.attr("href", response.docs[i].web_url)
-					    	.text(response.docs[i].web_url)
-					    	articles.append(a);
-
+					var convertedDate;
+					//assuming the average reading time is ~275wpm.
+					var avgWpm = 275;
+				
+				    for (var i = 0; i < numLimit; i++){
+				    	convertedDate = moment(new Date(response.docs[i].pub_date));
+				    	var wordCount = response.docs[i].word_count;
+				    	var wpmRead = Math.ceil( (wordCount/avgWpm) * 10);
+				    	numArticle++;
+				    	var articles = $("<div class='panel-body border displayArts'>")
+				    		.append( $("<i class='label label-primary divNum'>").text(numArticle) )
+				    		.append( $("<p class='wpm'>").text( wpmRead + " min read") )
+							.append( $("<h3 class='headline'>").text(response.docs[i].headline.main) )
+				   			.append( $("<h6>").text("Section: " + response.docs[i].section_name) )
+					  		.append( $("<h6>").text(response.docs[i].byline.original))
+					   		.append( $("<h6>").text( convertedDate.format("MMMM DD, YYYY") ) )
+							.append( $("<p>").text(response.docs[i].snippet) )
+				    		.append( $("<a>").attr("href", response.docs[i].web_url).text(response.docs[i].web_url) )
 				    	$("#TopArticles").append(articles);
 
 					// // console.log(data.response.docs[i].pub_date);
@@ -63,7 +55,8 @@ function nyApiQuery(searchTerm, startYear, endYear) {
 
 //Event Handlers
 //==================================================================================
-$("#search").on("click", function(){
+$("#search").on("click", function(event){
+	event.preventDefault();
 if( $("#searchTerm").val() === "") {
 	console.log("please enter something to search")
 	// add a warning message under search term
@@ -71,16 +64,17 @@ if( $("#searchTerm").val() === "") {
 else{
 //input conditionals for query
 //----------------------------------------------------------------------------------
-	var searchTerm = $("#searchTerm").val();
-	var startYear = $("#startYear").val();
-	var endYear = $("#endYear").val();
+	var searchTerm = $("#searchTerm").val().trim();
+	var startYear = $("#startYear").val().trim();
+	var endYear = $("#endYear").val().trim();
+	limit = $("#limit").val();
 
 	//regex to validate Start Range format of yyyy if it does not match a digit or is not in the fmt of yyyy will return a range of yyyy-0000 mm-00 and dd-00, will also return the same if ""
 	if ( startYear.match(/^\d/) && startYear.match(/^(20[0-2][0-9])$/) || startYear.match(/^\d/) && startYear.match(/^(19[0-9][0-9])$/) ) {
 	var startYear = $("#startYear").val() + "0101";
 	}
 		else {
-			startYear = "19700101" //set a start date instead
+			startYear = "19700101" //set a default start date of Jan 01 1970
 		}
 
 	//regex to validate End Range format of yyyy if it does not match a digit or is not in the fmt of yyyy will return a non-ranged yyyy-0000 mm-00 and dd-00, will also return the same if ""
@@ -101,15 +95,17 @@ else{
 	//remove id created on if statement<----needs to be created
 //nyApiQuery & passing the values of userinput values of searchTerm, startYear, & endYear
 //----------------------------------------------------------------------------------
-	nyApiQuery(searchTerm, startYear, endYear);
+	nyApiQuery(limit, searchTerm, startYear, endYear);
 }
+});
 //clears top articles and sets input val to ""
 $("#clear").on("click", function(){
+	numArticle = 0;
 	$(".displayArts").remove();
 	$("#searchTerm").val("");
 	$("#startYear").val("");
 	$("#endYear").val("");
 
-})
-
 });
+
+// });
